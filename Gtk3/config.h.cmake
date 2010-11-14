5a1767918ee2381c72fdf0d3d5c2020bcfac0652
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2010-11-14 21:27:38 +0000

    Rename

---
+++ Gtk3/config.h.cmake
@@ -0,0 +1,21 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define VERSION           "@QTCURVE_VERSION@"
+#define KDE3_ICONS_PREFIX "@KDE3_ICONS_PREFIX@"
+#define KDE4_ICONS_PREFIX "@KDE4_ICONS_PREFIX@"
+#define KDE3PREFIX        "@KDE3PREFIX@"
+#define KDE4PREFIX        "@KDE4PREFIX@"
+#define GTK_THEME_DIR     "@GTK_THEME_DIR@/gtk-3.0"
+
+#cmakedefine QTC_ADD_EVENT_FILTER
+#cmakedefine QTC_MODIFY_MOZILLA
+#cmakedefine QTC_MODIFY_MOZILLA_USER_JS
+#cmakedefine QTC_REORDER_GTK_DIALOG_BUTTONS
+#cmakedefine QTC_OLD_MOZILLA
+#cmakedefine QTC_DEFAULT_TO_KDE3
+#cmakedefine QTC_STYLE_SUPPORT
+#cmakedefine QTC_CAIRO_1_10_HACK
+#cmakedefine QTC_ENABLE_PARENTLESS_DIALOG_FIX_SUPPORT
+
+#endif
